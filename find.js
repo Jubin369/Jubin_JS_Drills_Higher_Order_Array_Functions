c1e56fd34968aaaac5f2249ad0c1ad4c2b5c0725
jubin369 <jubinayoob369@gmail.com>
2021-11-03 16:30:37 +0530

    Problem find for array is solved

--- find.js
+++ find.js
@@ -1,1 +1,13 @@
+const elements = [1, 2, 3, 4, 5, 5];
+let result = find(elements, 4);
 
+console.log("Element 4 is found at index " + result);
+
+function find(elements, key) {
+  for (i = 0; i < elements.length; i++) {
+    if (key == elements[i]) {
+      return i;
+    }
+  }
+  return;
+}
