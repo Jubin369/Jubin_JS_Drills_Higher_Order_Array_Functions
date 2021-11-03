149d25bca7051202ca2f257998e939b7f35bd4a6
jubin369 <jubinayoob369@gmail.com>
2021-11-03 16:47:46 +0530

    Problem filter for array is solved

--- filter.js
+++ filter.js
@@ -1,1 +1,21 @@
+const elements = [1, 2, 3, 4, 5, 5];
+let result = filter(elements, 4);
 
+console.log(result);
+
+// filter function checking values above 4;
+function filter(elements, key) {
+  let arr = [],
+    foundIndex = 0;
+
+  for (i = 0; i < elements.length; i++) {
+    if (elements[i] > key) {
+      arr[foundIndex++] = elements[i];
+    }
+  }
+
+  return arr;
+  // Do NOT use .filter, to complete this function.
+  // Similar to `find` but you will return an array of all elements that passed the truth test
+  // Return an empty array if no elements pass the truth test
+}
