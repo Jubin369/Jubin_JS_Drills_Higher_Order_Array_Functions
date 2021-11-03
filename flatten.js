3a9489a491e11a43ed0dd029fd557f05188cabaa
jubin369 <jubinayoob369@gmail.com>
2021-11-03 17:07:54 +0530

    Problem flatten for array is solved

--- flatten.js
+++ flatten.js
@@ -1,1 +1,17 @@
+const nestedArray = [1, [2], [[3]], [[[4]]]]; // use this to test 'flatten'
 
+let result = flatten(nestedArray);
+
+console.log(result);
+
+function flatten(ary) {
+  var ret = [];
+  for (var i = 0; i < ary.length; i++) {
+    if (Array.isArray(ary[i])) {
+      ret = ret.concat(flatten(ary[i]));
+    } else {
+      ret.push(ary[i]);
+    }
+  }
+  return ret;
+}
