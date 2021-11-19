a4cd16b29968d29bf8cb5f5b42a25b7b811dd844
jubin369 <jubinayoob369@gmail.com>
2021-11-19 10:11:25 +0530

    uptades for reduce

--- test/testReduce.js
+++ test/testReduce.js
@@ -1,1 +1,11 @@
+const items = [1, 2, 3, 4, 5, 5];
+const moduleReduce = require('../reduce');
 
+let startingValue=3;
+let myReduce = moduleReduce.reduce(items, function(acc, value){
+
+   return acc + value;
+   
+},startingValue);
+
+console.log(myReduce);
