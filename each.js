16b16bbc119ef3b8a99c80328767f18fd36380d8
Jubin Ayoob <jubinayoob369@gmail.com>
2021-11-19 09:15:51 +0530

    Update each.js

--- each.js
+++ each.js
@@ -1,17 +1,8 @@
-const elements = [1, 2, 3, 4, 5, 5];
-let result = each(elements);
 
-function each(elements) {
+function each(elements,cb) {
   for (i = 0; i < elements.length; i++) {
     cb(elements[i], i);
   }
-  // Do NOT use forEach to complete this function.
-  // Iterates over a list of elements, yielding each in turn to the `cb` function.
-  // This only needs to work with arrays.
-  // You should also pass the index into `cb` as the second argument
-  // based off http://underscorejs.org/#each
 }
 
-function cb(item, index) {
-  console.log(index + ":" + item + "\n");
-}
+module.exports = {each};
