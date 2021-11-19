b085047626f9a03fa22e4833b12261ec4630801e
jubin369 <jubinayoob369@gmail.com>
2021-11-19 10:02:02 +0530

    uptades for map

--- map.js
+++ map.js
@@ -1,21 +1,11 @@
-const elements = [1, 2, 3, 4, 5, 5];
-let result = map(elements);
 
-console.log(result);
-
-function map(elements) {
+function map(elements,cb) {
   let arr = [];
   for (i = 0; i < elements.length; i++) {
-    arr[i] = cb(elements[i]);
+    arr.push(cb(elements[i]));
   }
 
   return arr;
-  // Do NOT use .map, to complete this function.
-  // How map works: Map calls a provided callback function once for each element in an array, in order, and functionructs a new array from the res .
-  // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
-  // Return the new array.
 }
 
-function cb(item) {
-  return item * 3;
-}
+module.exports={map};
--- test/testMap.js
+++ test/testMap.js
@@ -1,1 +1,7 @@
+const items = [1, 2, 3, 4, 5, 5];
+const moduleMap=require('../map.js');
 
+let newResult= moduleMap.map(items,function(value){
+    return value+3;
+    });
+console.log(newResult );
