f64c5b199161f5e4490a51a0bafb9cbc5157a297
jubin369 <jubinayoob369@gmail.com>
2021-11-19 09:12:28 +0530

    Filter with callback functions

--- test/testFilter.js
+++ test/testFilter.js
@@ -1,1 +1,8 @@
 
+const items = [1, 2, 3, 4, 5, 5];
+const moduleFilter=require('../filter');
+
+let result = moduleFilter.filter(items,function(value){
+    return value<4;
+});
+console.log(result);
